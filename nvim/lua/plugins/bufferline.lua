36c38438f9cb4ab5bbcc6f4fcd481060b6318db2
fnj76 <wt1679d@outlook.com>
2024-07-24 07:31:41 -0500

    adding nvim and zsh settings

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        always_show_bufferline = true,
+        show_close_icon = false,
+      },
+    },
+  },
+}
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add solarized-osaka
+  { "craftzdog/solarized-osaka.nvim" },
+
+  -- Configure LazyVim to load solarized-osaka
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  }
+}
