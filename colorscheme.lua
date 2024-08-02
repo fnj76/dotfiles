729e24a655b2e7cde84175641cea8a27938145bc
Fabián <108005328+fnj76@users.noreply.github.com>
2024-08-01 19:02:18 -0500

    r1

---
+++ colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+  -- add tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  -- Configure LazyVim to load tokyonight
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-storm",
+    },
+  },
+}
